8a56deb69fc90a1b18991302ac3e66975f901c8b
Rohan Pandey <rohan_pandey@mckinsey.com>
2022-02-08 21:11:37 +0530

    day5 algorithms1

--- 876. Middle of the Linked List.cpp
+++ 876. Middle of the Linked List.cpp
@@ -75,3 +75,33 @@ public:
         return middle_ptr;
     }
 };
+
+
+//Approach-3
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int count=0;
+        struct ListNode* middle_ptr;
+        middle_ptr=head;
+        while(head!=nullptr)
+        {
+            head=head->next;
+            count+=1;
+            if(count%2==0)
+                middle_ptr=middle_ptr->next;
+        }
+        return middle_ptr;
+    }
+};
